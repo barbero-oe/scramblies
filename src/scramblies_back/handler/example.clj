dd0d4ad1d38485e3c29644cbe4bd84968eb5ef1c
barbero-oe <barbero.oe@gmail.com>
2021-03-30 21:35:20 -0300

    Initial commit

---
+++ src/scramblies_back/handler/example.clj
@@ -0,0 +1,8 @@
+(ns scramblies-back.handler.example
+  (:require [ataraxy.core :as ataraxy]
+            [ataraxy.response :as response] 
+            [integrant.core :as ig]))
+
+(defmethod ig/init-key :scramblies-back.handler/example [_ options]
+  (fn [{[_] :ataraxy/result}]
+    [::response/ok {:example "data"}]))
