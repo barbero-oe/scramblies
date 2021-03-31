08858d1a0bd4066240685bf35b4b523e9ff4451a
barbero-oe <barbero.oe@gmail.com>
2021-03-30 23:47:34 -0300

    Adds tests cases for the scramble? function

--- test/scramblies_back/domain/scramble_test.clj
+++ test/scramblies_back/domain/scramble_test.clj
@@ -3,9 +3,11 @@
             [scramblies-back.domain.scramble :refer [scramble?]]))
 
 (deftest scramble-test
-  (testing "tells if characters of first string can be rearranged to form second string"
+  (testing "checks if the characters of the first string can be rearranged to form the second string"
     (is (false? (scramble? "abc" "def")))
     (is (true? (scramble? "dlrow" "world")))
-    (is (true? (scramble? "helloworld" "world")))))
-
-
+    (is (true? (scramble? "helloworld" "world")))
+    (is (true? (scramble? "rekqodlw" "world")))
+    (is (true? (scramble? "cedewaraaossoqqyt" "codewars")))
+    (is (false? (scramble? "katas" "steak")))
+    (is (false? (scramble? "kata" "katas")))))
